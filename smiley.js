72bdad019aa4975a738564113b806d5a28ccf1dd
Ivar van den Bosch <78146502+IvarvandenBosch@users.noreply.github.com>
2022-07-06 23:36:10 +0200

    [💄] Smiley feedback method
    Smiley feedback method with gradient animate background

---
+++ smiley.js
@@ -0,0 +1,7 @@
+let grinFace = document.getElementById('grin')
+let mehFace = document.getElementById('meh')
+let frownFace = document.getElementById('frown')
+let container = document.getElementById('container')
+function nextPage() {
+    
+}
