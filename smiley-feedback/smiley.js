c4c429aa1203421089c72e32501ee97719a460cd
Firecarr0t <dagelijksvloggamen@gmail.com>
2022-07-07 00:33:22 +0200

    [💄] Added animations
    Added thank you page, still in need of animations.

---
+++ smiley-feedback/smiley.js
@@ -0,0 +1,10 @@
+let grinFace = document.getElementById('grin')
+let mehFace = document.getElementById('meh')
+let frownFace = document.getElementById('frown')
+let container = document.getElementById('container')
+let containerTy = document.getElementById('thankyou') 
+
+function nextPage() {
+    container.classList.toggle('anim')
+    containerTy.classList.toggle('anim')
+}
--- smiley.js
+++
@@ -1,7 +0,0 @@
-let grinFace = document.getElementById('grin')
-let mehFace = document.getElementById('meh')
-let frownFace = document.getElementById('frown')
-let container = document.getElementById('container')
-function nextPage() {
-    
-}
